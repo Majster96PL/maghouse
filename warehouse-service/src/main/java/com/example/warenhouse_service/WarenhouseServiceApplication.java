f2d1ef9af776cbc5d625c20ebbc6db5e4daba436
Majster96PL <piotruniok96@gmail.com>
2024-06-15 20:54:30 +0200

    Add module warehouse

---
+++ warehouse-service/src/main/java/com/example/warenhouse_service/WarenhouseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.warenhouse_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenhouseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarenhouseServiceApplication.class, args);
+	}
+
+}
---
+++ warehouse-service/src/test/java/com/example/warenhouse_service/WarenhouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warenhouse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarenhouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
