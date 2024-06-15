82e17081eba6caf48a382a6dfc69a112c7c649e7
Majster96PL <piotruniok96@gmail.com>
2024-06-15 18:45:16 +0200

    Testing endpoint for instance of eureka server

--- eureka-server/src/test/java/com/example/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/eureka_server/EurekaServerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.eureka_server;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void eurekaHomePageIsAccessible() {
+		RestAssured.port = port;
+		given().when().get("/").then().statusCode(200).body(containsString("Eureka"));
 	}
 
 }
