4467d3c8848ab8cb5ccfddfb1476a1b1725e863f
Majster96PL <piotruniok96@gmail.com>
2024-06-15 21:33:43 +0200

    Configuration warehouse module as eureka client

--- warehouse-service/src/main/java/com/example/warenhouse_service/WarehouseServiceApplication.java
+++ warehouse-service/src/main/java/com/example/warenhouse_service/WarehouseServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.warenhouse_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class WarenhouseServiceApplication {
+@EnableDiscoveryClient
+public class WarehouseServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarenhouseServiceApplication.class, args);
+		SpringApplication.run(WarehouseServiceApplication.class, args);
 	}
 
 }
