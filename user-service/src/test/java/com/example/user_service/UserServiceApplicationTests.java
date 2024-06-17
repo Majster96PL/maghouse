f84d35343a8553c6da7dfe0b9f42bbd0b20f044b
Majster96PL <piotruniok96@gmail.com>
2024-06-17 12:41:07 +0200

    Testing configuration eureka client for user module

--- user-service/src/test/java/com/example/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/example/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.user_service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.lenient;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserServiceApplicationTests {
 
+	@Mock
+	private EurekaClientConfigBean eurekaClientConfigBean;
+
+	@Mock
+	private EurekaServiceRegistry eurekaServiceRegistry;
+
 	@Test
-	void contextLoads() {
+	void eurekaClientConfigurationLoads() {
+		lenient().when(eurekaClientConfigBean.getServiceUrl())
+				.thenReturn(Map.of("defaultZone", "http://localhost:8761/eureka/"));
+		assertNotNull(eurekaClientConfigBean, "Eureka Client Config Bean should not be null");
 	}
 
+	@Test
+	void eurekaServerRegistryLoads() {
+		assertNotNull(eurekaServiceRegistry, "Eureka Service Registry should not be null");
+	}
+
+	@Test
+	void eurekaClientPropertiesTest() {
+		lenient().when(eurekaClientConfigBean.getServiceUrl())
+				.thenReturn(Map.of("defaultZone", "http://localhost:8761/eureka/"));
+		assertNotNull(eurekaClientConfigBean.getServiceUrl(), "Service URL should not be null");
+		assertNotNull(eurekaClientConfigBean.getServiceUrl()
+				.get("defaultZone"), "Default Zone URL should not be null ");
+	}
 }
