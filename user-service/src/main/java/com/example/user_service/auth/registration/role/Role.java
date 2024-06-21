7c11722fe6ab90ee0e06cfbf11e737a85a6031d9
Majster96PL <piotruniok96@gmail.com>
2024-06-21 22:05:13 +0200

    Implementation authorities method for users

--- user-service/src/main/java/com/example/user_service/auth/registration/role/Role.java
+++ user-service/src/main/java/com/example/user_service/auth/registration/role/Role.java
@@ -2,8 +2,11 @@ package com.example.user_service.auth.registration.role;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public enum Role {
@@ -31,4 +34,12 @@ public enum Role {
     @Getter
     private final Set<Permission> permissions;
 
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
 }
