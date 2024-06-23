3bf6ac389f835114bacd698e390b704dd902c078
Majster96PL <piotruniok96@gmail.com>
2024-06-23 14:02:23 +0200

    Create validation for users

--- user-service/src/main/java/com/example/user_service/auth/login/jwt/JWTFilter.java
+++ user-service/src/main/java/com/example/user_service/auth/login/jwt/JWTFilter.java
@@ -5,6 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +18,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -26,5 +34,20 @@ public class JWTFilter extends OncePerRequestFilter {
             return;
         }
         final String jwt = header.substring(7);
+        final String userEmail = jwtService.extractUserEmail(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isValidToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
     }
 }
--- user-service/src/main/java/com/example/user_service/auth/login/jwt/JwtService.java
+++ user-service/src/main/java/com/example/user_service/auth/login/jwt/JwtService.java
@@ -37,6 +37,20 @@ public class JwtService {
                 .signWith(getSigntInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername())) && !isExpiredToken(token);
+    }
+
+    private boolean isExpiredToken(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
