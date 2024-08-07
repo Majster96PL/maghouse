ddfa347227d70442a8cfa2ea942953f6ca0d167d
Majster96PL <piotruniok96@gmail.com>
2024-08-07 18:04:55 +0200

    Create model item

---
+++ items-service/src/main/java/com/example/items_service/item/Item.java
@@ -0,0 +1,34 @@
+package com.example.items_service.item;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+
+@Entity
+@Builder
+@Data
+@Table(name = "items")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name required!")
+    @Size(max = 50)
+    private String name;
+    @Size(max = 50)
+    @NotBlank(message = "Code required!")
+    private String itemCode;
+    private int quantity;
+    @ManyToOne
+    @JoinColumn(name = "firstname", nullable = false)
+    private User firstname;
+
+}
