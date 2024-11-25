ca30b94f709606fa58d4b96b70511d214ffb9d1e
Majster96PL <piotruniok96@gmail.com>
2024-11-25 19:28:40 +0100

    Create model delivery with status

---
+++ src/main/java/com/example/maghouse/delivery/Delivery.java
@@ -0,0 +1,39 @@
+package com.example.maghouse.delivery;
+
+import com.example.maghouse.item.Item;
+import com.example.maghouse.warehouse.Warehouse;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "delivery" )
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Supplier required!")
+    @Size(max = 50)
+    private String supplier;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    private String numberDelivery;
+    private String itemName;
+    private String itemCode;
+    private int quantity;
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+    private String warehouseLocation;
+
+}
---
+++ src/main/java/com/example/maghouse/delivery/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.example.maghouse.delivery;
+
+public enum DeliveryStatus {
+    PENDING, IN_PROGRESS, DELIVERED, CANCELLED
+}
