3090cc2ac8429c40884525e9636fe166ec3b340b
Majster96PL <piotruniok96@gmail.com>
2024-12-01 23:32:36 +0100

    Add path for delivery in security config

--- src/main/java/com/example/maghouse/security/SecurityConfig.java
+++ src/main/java/com/example/maghouse/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                         auth.requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("auth/item/**").hasAnyRole("USER", "ADMIN", "MANAGER", "WAREHOUSEMAN", "DRIVER")
                                 .requestMatchers("auth/warehouse/**").hasAnyRole("USER", "ADMIN", "MANAGER", "WAREHOUSEMAN", "DRIVER")
+                                .requestMatchers("auth/delivery/**").hasAnyRole("USER", "ADMIN", "MANAGER", "WAREHOUSEMAN", "DRIVER")
                                 .requestMatchers("auth/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
