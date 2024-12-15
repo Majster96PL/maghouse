0066935e3408d31eb6331e257df6a48659e91ceb
Majster96PL <piotruniok96@gmail.com>
2024-12-15 16:02:22 +0100

    Update dependencies for tests

--- src/test/java/com/example/maghouse/MagHouseApplicationTests.java
+++ src/test/java/com/example/maghouse/MagHouseApplicationTests.java
@@ -1,22 +1,11 @@
 package com.example.maghouse;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Suite
-@IncludeEngines("junit-jupiter")
 @SpringBootTest
-@SelectPackages({
-		"com.examples.maghouse.unit",
-		"com.examples.maghouse.integration"
-})
 class MagHouseApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void contextLoads(){
 	}
-
 }
