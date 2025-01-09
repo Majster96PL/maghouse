69ed9dd3f1a24f7bfd0db561c2a2e636fcb68bfb
Majster96PL <piotruniok96@gmail.com>
2025-01-09 21:24:40 +0100

    Add Validation for user request

--- src/main/java/com/example/maghouse/auth/registration/user/UserRequest.java
+++ src/main/java/com/example/maghouse/auth/registration/user/UserRequest.java
@@ -2,6 +2,9 @@ package com.example.maghouse.auth.registration.user;
 
 
 import com.example.maghouse.auth.registration.role.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +15,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserRequest {
+
+    @NotBlank(message = "Firstname required!")
     private String firstname;
+    @NotBlank(message = "Lastname required!")
     private String lastname;
+    @Email
+    @NotBlank(message = "Email required!")
     private String email;
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
     private Role role;
 
