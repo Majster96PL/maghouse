658245c57794bcfc7e6c289ba3d76779c9495dfe
Majster96PL <piotruniok96@gmail.com>
2025-01-09 21:51:01 +0100

    Add  handler for unauthorized status

--- src/main/java/com/example/maghouse/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/maghouse/auth/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.maghouse.auth.exception;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,5 +24,12 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return errors;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
