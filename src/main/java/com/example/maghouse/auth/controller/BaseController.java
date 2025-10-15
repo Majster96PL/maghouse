342efe0b997bf2ef9fb40665d20c8af9b612fc97
Majster96PL <piotruniok96@gmail.com>
2025-10-15 13:20:40 +0200

    feat: add BaseController to handle authentication in controller layer

---
+++ src/main/java/com/example/maghouse/auth/controller/BaseController.java
@@ -0,0 +1,28 @@
+package com.example.maghouse.auth.controller;
+
+import com.example.maghouse.auth.registration.user.User;
+import com.example.maghouse.auth.registration.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+public abstract class BaseController {
+
+    protected final UserService userService;
+
+    protected BaseController(UserService userService) {
+        this.userService = userService;
+    }
+
+    protected User getAuthenticateUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            log.warn("Authentication failed - user not authenticated!");
+            throw new SecurityException("Authentication failed - user not authenticated!");
+        }
+        String email = authentication.getName();
+        log.debug("User authenticated: {}", email);
+        return userService.findByEmail(email);
+    }
+}
